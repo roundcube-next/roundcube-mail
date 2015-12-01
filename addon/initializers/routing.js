db1971b950d63e38e1086ec99359ecb6d4a5306e
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-12-01 17:21:03 +0100

    Advertise "main" route and set up redirects

--- addon/initializers/routing.js
+++ addon/initializers/routing.js
@@ -1,17 +1,19 @@
 export function initialize(application) {
-  // Access the application router and register mail routes
-  var router = application.resolve('router:main');
+  var router = application.resolve('router:main'),
+      apps = application.lookup('service:apps');
 
+  // Register all routes on the application router
   router.map(function () {
     this.route('shell', { path: '/' }, function () {
       this.route('mail', { path: '/mail' }, function () {
-        this.route('index', { path: '/' });
         this.route('mailbox', { path: '/:mailbox_name'}, function () {
           this.route('message', { path: '/:message_id' });
         });
       });
     });
   });
+
+  apps.register('Mail', 'shell.mail');
 }
 
 export default {
--- addon/shell/mail/route.js
+++ addon/shell/mail/route.js
@@ -3,5 +3,24 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.modelFor('shell').getMailboxes();
+  },
+
+  redirect: function(model, transition) {
+    if (transition.targetName === 'shell.mail.index') {
+      var inboxName = _.result(_.find(model, function(mailbox) {
+        return mailbox.role && mailbox.role.value === 'inbox';
+      }), 'name');
+
+      // If a mailbox with the role 'inbox' is found, redirect to it by default
+      if (inboxName) {
+        this.transitionTo('shell.mail.mailbox', inboxName);
+      }
+    }
+  },
+
+  actions: {
+    willTransition: function (transition) {
+      this.redirect(this.context, transition);
+    }
   }
 });
