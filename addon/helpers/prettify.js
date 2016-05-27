53db664e0e4c2d61bd0273e600a4a4bb09442121
Thomas Bruederli <thomas@roundcube.net>
2016-05-27 19:19:37 +0200

    Remove useless prettify helper
    This helper tried to fix wrongly encoded strings coming from the JMAP server.
    The double encoding this helper wants to reverse is now fixed in the jmap-perl proxy.
    
    Anyway, it's not the the client's job to work around invalid data and broken server responses.

--- addon/helpers/prettify.js
+++
@@ -1,8 +0,0 @@
-/* global escape */
-import Ember from 'ember';
-
-export function prettify(params) {
-  return decodeURIComponent(escape(params[0]));
-}
-
-export default Ember.Helper.helper(prettify);
--- app/prettify/helper.js
+++
@@ -1,1 +0,0 @@
-export { default, prettify } from 'roundcube-mail/helpers/prettify';
